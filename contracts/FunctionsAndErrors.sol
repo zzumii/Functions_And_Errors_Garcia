583dbce40c232f0b3c587ce19aa6a2df02df3a50
adam garcia <auegarcia@mymail.mapua.edu.ph>
2023-07-20 10:15:02 +0000

    commit

--- contracts/FunctionsAndErrors.sol
+++ contracts/FunctionsAndErrors.sol
@@ -4,17 +4,17 @@ pragma solidity ^0.8.17;
 contract FunctionsAndErrors {
     uint public Odd;
 
-    function requireOddNumber(uint number) external {
+    function dorequireOdd(uint number) external {
         require(number % 2 == 1, "You inputted Even");
         ++Odd;
     }
 
-    function assertOddNumber(uint number) external {
+    function doassertOdd(uint number) external {
         assert(number % 2 == 1);
         ++Odd;
     }
 
-    function revertOddNumber(uint number) external {
+    function dorevertOdd(uint number) external {
         if (number % 2 == 0) {
             revert("You inputted even");
         }
